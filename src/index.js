ba85a272c1668485e955b087aa4c3fb6350012f7
ZeroIDdev <115116337+ZeroIDdev@users.noreply.github.com>
2024-01-22 05:37:01 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import routes from "./routes/index.js";
 
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Mengizinkan semua domain
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use(routes);
 
